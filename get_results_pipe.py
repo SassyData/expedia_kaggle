45d4462ed9436accd55d80ecef48adcf0dcec471
SassyData <tabithajohnston@live.co.uk>
2021-04-09 13:44:35 +0200

    code: added get_results_pipe which can be run from terminal usngsys.argv

--- get_results_pipe.py
+++ get_results_pipe.py
@@ -1,35 +1,48 @@
 import pandas as pd
 import sys
-from proj_fns import preprocess_step, create_submission
+from proj_fns import create_submission
 
 """create submission useing sys.argv variables (model variables / submission name / model file)
 e.g. if model variables = "models/top_12_feats.csv" + submission name = "rf_12vars.csv" + model = "rf_model.sav" 
 then from terminal..  
-$ python get_results_pipe.py "models/top_12_feats.csv" "rf_12vars.csv" "rf_model.sav"
+$ python get_results_pipe.py "models/top_12_feats.csv" "new_submission_name.csv" "rf_model.sav"
  
  (model vars could also be None)
  """
+
+# Set-up args
+model_vars_file = sys.argv[1]
+sub_file_name = sys.argv[2]
+model_file = sys.argv[3]
 
 # Raw data load
 test_raw = pd.read_csv('data/test.csv')
 
 # Load top 12 vars df
-if sys.argv[0]:
-    model_vars = pd.read_csv(sys.argv[0])['0'].tolist()
+if model_vars_file:
+    model_vars = pd.read_csv(model_vars_file)['0'].tolist()
     print(f"Model variables loaded: \n{model_vars}")
+else:
+    model_vars = None
 
 # RF submission
-create_submission(test_raw, model_vars = sys.argv[0], sub_file_name = sys.argv[1], model_file = sys.argv[2])
+create_submission(test_raw, model_vars, sub_file_name, model_file)
 
 
 if __name__ == '__main__':
+    # Load test set
     test_raw = pd.read_csv('data/test.csv')
 
+    # Set-up args
+    model_vars_file = "models/top_12_feats.csv"
+    sub_file_name = "test_main_fn.csv"
+    model_file = "rf_model.sav"
+
     # Load top 12 vars df
-    model_vars = pd.read_csv("models/top_12_feats.csv")['0'].tolist()
+    model_vars = pd.read_csv(model_vars_file)['0'].tolist()
     print(f"Model variables loaded: \n{model_vars}")
 
     # RF submission
-    create_submission(test_raw, model_vars, sub_file_name = "test_main_fn.csv", model_file = "rf_model.sav")
+    create_submission(test_raw, model_vars, sub_file_name, model_file)
 
 
